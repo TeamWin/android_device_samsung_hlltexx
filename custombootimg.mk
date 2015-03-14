6dbe3d1401477743d3f0c38282a6d4b3790eae00
jenkins <jenkins@teamw.in>
2015-03-14 18:42:09 -0400

    updates

--- custombootimg.mk
+++ custombootimg.mk
@@ -4,9 +4,10 @@ $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
 		$(recovery_ramdisk) \
 		$(recovery_kernel)
 	@echo ----- Creating ramdisk ------
-	rm -f out/target/product/hlltexx/recovery/root/init
-	cp device/samsung/hlltexx/recovery/init out/target/product/hlltexx/recovery/root/init
+	#rm -f out/target/product/hlltexx/recovery/root/init
+	#cp device/samsung/hlltexx/recovery/init out/target/product/hlltexx/recovery/root/init
 	chmod 644 out/target/product/hlltexx/recovery/root/init.rc
+	chmod 644 out/target/product/hlltexx/recovery/root/init.recovery.universal5420.rc
 	chmod 644 out/target/product/hlltexx/recovery/root/default.prop
 	(cd out/target/product/hlltexx/recovery/root/ && find * | sort | cpio -o -H newc) | gzip > $(recovery_ramdisk)
 	@echo ----- Making recovery image ------
