86bfcde2bbd8da847bbc18200b0b0333b57b8a71
bigbiff <bigbiff@teamw.in>
2016-08-13 14:48:26 -0400

    Update for building on build.twrp.me.

--- custombootimg.mk
+++ custombootimg.mk
@@ -1,4 +1,4 @@
-MKBOOTIMG_BIN := out/host/linux-x86/bin/mkbootimg
+MKBOOTIMG_BIN := mkbootimg
 
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
 		$(recovery_ramdisk) \
@@ -6,10 +6,10 @@ $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
 	@echo ----- Creating ramdisk ------
 	#rm -f out/target/product/hlltexx/recovery/root/init
 	#cp device/samsung/hlltexx/recovery/init out/target/product/hlltexx/recovery/root/init
-	chmod 644 out/target/product/hlltexx/recovery/root/init.rc
-	chmod 644 out/target/product/hlltexx/recovery/root/init.recovery.universal5420.rc
-	chmod 644 out/target/product/hlltexx/recovery/root/default.prop
-	(cd out/target/product/hlltexx/recovery/root/ && find * | sort | cpio -o -H newc) | gzip > $(recovery_ramdisk)
+	#chmod 644 out/target/product/hlltexx/recovery/root/init.rc
+	#chmod 644 out/target/product/hlltexx/recovery/root/init.recovery.universal5420.rc
+	#chmod 644 out/target/product/hlltexx/recovery/root/default.prop
+	(cd $(OUT)/target/product/hlltexx/recovery/root/ && find * | sort | cpio -o -H newc) | gzip > $(recovery_ramdisk)
 	@echo ----- Making recovery image ------
 	$(MKBOOTIMG_BIN) --kernel $(TARGET_PREBUILT_KERNEL) --ramdisk $(recovery_ramdisk) --base $(BOARD_KERNEL_BASE) --pagesize $(BOARD_KERNEL_PAGESIZE) $(BOARD_MKBOOTIMG_ARGS) --output $@
 	@echo ----- Made recovery image -------- $@
